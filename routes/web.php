6d0101f7813ce4eaf6372dcf6b3e09f5a87efc83
DanielAbrahamDzulPech <danieldayamond@gmail.com>
2022-06-27 13:18:38 -0500

    aunteticador add

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
